83267f8578da5c8fa825b25d026290bd0a2985aa
Naga <padalanagalakshmi79416@gmail.com>
2026-05-14 00:20:30 +0530

    Add file size formatting and processing summary

--- script.py
+++ script.py
@@ -1,6 +1,14 @@
 import os
 import pandas as pd
 
+def formate_size(size):
+    if size < 1024:
+        return f"{size} B"
+    elif size < 1024 * 1024:
+        return f"{round(size / 1024, 2)} KB"
+    else:
+        return f"{round(size / 1024 * 1024)} KB"
+
 base_folder = "data"
 rows = []
 
@@ -11,17 +19,22 @@ for folder_name in os.listdir(base_folder):
         for file in os.listdir(folder_path):
             file_path = os.path.join(folder_path, file)
             file_name, extension = os.path.splitext(file)
+            file_size = os.path.getsize(file_path)
+            formatted_size = formate_size(file_size)
 
             if extension.lower() in [".jpg", ".txt"]:
 
                 rows.append({
                     "Category": folder_name,
                     "File": file_name,
-                    "Extension": extension
+                    "Extension": extension,
+                    "File Size": formatted_size
                     })
+total_files = len(rows)
 
 df = pd.DataFrame(rows)
 
 df.to_excel("output/output.xlsx", index = False)
 
 print("Excel File Created successfully!")
+print(f"Total files processed: {total_files}")
